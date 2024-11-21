6b47835c1f0f60894522201ab208ad0ebb2b25fe
JanuaryToribioJr <toribiojanuaryjr@gmail.com>
2024-11-22 00:21:10 +0800

    Add project files.

---
+++ Inventory_/frmAddProduct.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Inventory_
+{
+    public partial class frmAddProduct : Form
+    {
+        private string _ProductName;
+        private string _Category;
+        private string _MfgDate;
+        private string _ExpDate;
+        private string _Description;
+        private int _Quantity;
+        private double _SellPrice;
+        private BindingSource showProductList;
+
+        public frmAddProduct()
+        {
+            InitializeComponent();
+            showProductList = new BindingSource();
+        }
+        private string Product_Name(string name)
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+                throw new StringFormatException("Invalid input of words.");
+            return name;
+        }
+        private int Quantity(string qty)
+        {
+            if (!Regex.IsMatch(qty, @"^[0-9]"))
+                throw new NumberFormatException("Invalid quantity.");
+            return Convert.ToInt32(qty);
+        }
+
+        private double Selling_Price(string price)
+        {
+            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+                throw new CurrencyException("Invalid amount.");
+            return Convert.ToDouble(price);
+        }
+        private void frmAddProduct_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProductCategory = { "Beverages", "Bread / Bakery", "Canned / Jarred Goods", "Dairy", "Frozen Goods", "Meat", "Personal Care", "Other" };
+
+            foreach (string List in ListOfProductCategory)
+            {
+                cbCategory.Items.Add(List);
+            }
+        }
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _ProductName = Product_Name(txtProductName.Text);
+                _Category = cbCategory.Text;
+                _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
+                _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
+                _Description = richTxtDescription.Text;
+                _Quantity = Quantity(txtQuantity.Text);
+                _SellPrice = Selling_Price(txtSellPrice.Text);
+                showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate,
+                _ExpDate, _SellPrice, _Quantity, _Description));
+                gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                gridViewProductList.DataSource = showProductList;
+            }
+            catch (StringFormatException ee)
+            {
+                MessageBox.Show($"String Format Error: {ee.Message}", "Invalid input");
+            }
+            catch (NumberFormatException ee)
+            {
+                MessageBox.Show($"Number Format Error: {ee.Message}", "Invalid input");
+            }
+            catch (CurrencyException ee)
+            {
+                MessageBox.Show($"Currency Format Error: {ee.Message}", "Invalid input");
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show($"Unexpected Error: {ee.Message}", "Not applicable.\nPlease check and try again.");
+            }
+            finally
+            {
+                MessageBox.Show("Recorded");
+            }
+        }
+    }
+    public class ProductClass
+    {
+        private int _Quantity;
+        private double _SellingPrice;
+        private string _ProductName, _Category, _ManufacturingDate, _ExpirationDate, _Description;
+        public ProductClass(string ProductName, string Category, string MfgDate, string ExpDate,
+        double Price, int Quantity, string Description)
+        {
+            this._Quantity = Quantity;
+            this._SellingPrice = Price;
+            this._ProductName = ProductName;
+            this._Category = Category;
+            this._ManufacturingDate = MfgDate;
+            this._ExpirationDate = ExpDate;
+            this._Description = Description;
+        }
+        public string productName
+        {
+            get
+            {
+                return this._ProductName;
+            }
+            set
+            {
+                this._ProductName = value;
+            }
+        }
+        public string category
+        {
+            get
+            {
+                return this._Category;
+            }
+            set
+            {
+                this._Category = value;
+            }
+        }
+        public string manufacturingDate
+        {
+            get
+            {
+                return this._ManufacturingDate;
+            }
+            set
+            {
+                this._ManufacturingDate = value;
+            }
+        }
+        public string expirationDate
+        {
+            get
+            {
+                return this._ExpirationDate;
+            }
+            set
+            {
+                this._ExpirationDate = value;
+            }
+        }
+        public string description
+        {
+            get
+            {
+                return this._Description;
+            }
+            set
+            {
+                this._Description = value;
+            }
+        }
+        public int quantity
+        {
+            get
+            {
+                return this._Quantity;
+            }
+            set
+            {
+                this._Quantity = value;
+            }
+        }
+        public double sellingPrice
+        {
+            get
+            {
+                return this._SellingPrice;
+            }
+            set
+            {
+                this._SellingPrice = value;
+            }
+        }
+    }
+
+    public class StringFormatException : Exception
+    {
+        public StringFormatException(string message) : base(message) { }
+    }
+    public class NumberFormatException : Exception 
+    {
+        public NumberFormatException(string message) : base(message) { }
+    }
+    public class CurrencyException : Exception
+    {
+        public CurrencyException(string message) : base(message) { }
+    }
+}
